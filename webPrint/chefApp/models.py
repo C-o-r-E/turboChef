c2296573ef4f9cb61efbf00eb04c516d5f55bc66
C-o-r-E <can.of.tuna@gmail.com>
2013-12-02 13:30:38 -0500

    trying to get the models right

--- webPrint/chefApp/models.py
+++ webPrint/chefApp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+#Note: for transition to python3, may need to change __unicode__ to __str__
+
 class Printer(models.Model):
 	name = models.CharField(max_length=200)
 	percent_complete = models.IntegerField(default=0)
@@ -10,18 +12,22 @@ class Printer(models.Model):
 	def __unicode__(self):
 		return self.name
 
+
+class Material(models.Model):
+	name = models.CharField(max_length=200)
+	
+	def __unicode__(self):
+		return self.name
+
+
 class Extruder(models.Model):
 	printer = models.ForeignKey(Printer)
 	current_temp = models.IntegerField(default=0)
 	max_temp = models.IntegerField(default=0)
 	name = models.CharField(max_length=200)
+	material = models.OneToOneField(Material)
 
 	def __unicode__(self):
 		return self.name
 
-class Material(models.Model):
-	extruder = models.OneToOneField(Extruder)
-	name = models.CharField(max_length=200)
-	
-	def __unicode__(self):
-		return self.name
+
