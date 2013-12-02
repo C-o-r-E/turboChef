aedc42992fd5d3489b9fec5760e85b6a08897628
C-o-r-E <can.of.tuna@gmail.com>
2013-12-02 12:02:22 -0500

    started chefApp, added initial models

---
+++ webPrint/chefApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Printer(models.Model):
+	name = models.CharField(max_length=200)
+	percent_complete = models.IntegerField(default=0)
+	state = models.CharField(max_length=200)
+	last_start = models.DateTimeField('Last started running')
+	last_stop = models.DateTimeField('Last stopped running')
+
+class Extruder(models.Model):
+	printer = models.ForeignKey(Printer)
+	current_temp = models.IntegerField(default=0)
+	max_temp = models.IntegerField(default=0)
+
+class Material(models.Model):
+	extruder = models.OneToOneField(Extruder)
+	name = models.CharField(max_length=200)
