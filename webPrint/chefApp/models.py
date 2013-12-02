2673e79876410b7f759a507ff27bbf0ec4352c3d
C-o-r-E <can.of.tuna@gmail.com>
2013-12-02 12:21:32 -0500

    update models to be string readable

--- webPrint/chefApp/models.py
+++ webPrint/chefApp/models.py
@@ -7,11 +7,21 @@ class Printer(models.Model):
 	last_start = models.DateTimeField('Last started running')
 	last_stop = models.DateTimeField('Last stopped running')
 
+	def __unicode__(self):
+		return self.name
+
 class Extruder(models.Model):
 	printer = models.ForeignKey(Printer)
 	current_temp = models.IntegerField(default=0)
 	max_temp = models.IntegerField(default=0)
+	name = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.name
 
 class Material(models.Model):
 	extruder = models.OneToOneField(Extruder)
 	name = models.CharField(max_length=200)
+	
+	def __unicode__(self):
+		return self.name
