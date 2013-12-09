217b58947dc389515178cf64470f42b06248dc06
C-o-r-E <can.of.tuna@gmail.com>
2013-12-09 10:26:45 -0500

    server seems to work as long as host.com/print/ is used instead of host.com/print

--- webPrint/webPrint/settings.py
+++ webPrint/webPrint/settings.py
@@ -4,6 +4,8 @@ from os import path
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+#FORCE_SCRIPT_NAME = ''
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -24,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
