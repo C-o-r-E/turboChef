e3aa507174817d8fcec897336616f1dfce6f30de
C-o-r-E <can.of.tuna@gmail.com>
2013-12-20 23:01:58 -0500

    added invocation of slic3r (hardcoded for now)

--- webPrint/chefApp/slice.py
+++ webPrint/chefApp/slice.py
@@ -1,13 +1,30 @@
 from chefApp.models import Printer, Extruder, Material, CADFile
 import threading
 from time import sleep
+from django.conf import settings
+import subprocess
+
+sliceFail = False
 
 #Function for managing slicing
 def sliceMan(stlFile):
     #print "sliceMan: %s" % stlFile.name
     stlFile.status_msg = "Uploaded: Waiting for slicing..."
+    stlFile.full_path = stlFile.cadfile.path
+    stlFile.path_to_gcode = settings.GCODE_PATH + stlFile.name + '.gcode'
     stlFile.save()
     
+
+    #now we actually invoke slic3r
+    try:
+        subprocess.check_output(
+            ['perl', settings.SLIC3R_PATH, stlFile.cadfile.path, '-o', stlFile.path_to_gcode]
+        )
+    except subprocess.CalledProcessError:
+        print "Error slicing " + stlFile.name
+        sliceFail = True
+        stlFile.status_msg = "Error: Slicing failed"
     
-    stlFile.status_msg = "Done: ready for printing"
+    if sliceFail == False:
+        stlFile.status_msg = "Done: ready for printing"
     stlFile.save()
--- webPrint/webPrint/settings.py
+++ webPrint/webPrint/settings.py
@@ -55,6 +55,13 @@ USE_TZ = True
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = '/home/corey/git/turboChef/webPrint/webPrint/media/'
 
+##################################
+# custom stuff for the chef
+##################################
+GCODE_PATH = '/home/corey/git/turboChef/webPrint/webPrint/gcode/'
+SLIC3R_PATH = '/home/corey/git/Slic3r/slic3r.pl'
+
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
