56ab4b2124c4fbca4cb5ec35fb921676e2e12370
C-o-r-E <can.of.tuna@gmail.com>
2013-12-20 20:24:21 -0500

    remove timertest code

--- webPrint/chefApp/slice.py
+++ webPrint/chefApp/slice.py
@@ -4,14 +4,10 @@ from time import sleep
 
 #Function for managing slicing
 def sliceMan(stlFile):
-    print "sliceMan: %s" % stlFile.name
+    #print "sliceMan: %s" % stlFile.name
     stlFile.status_msg = "Uploaded: Waiting for slicing..."
     stlFile.save()
     
-    for r in range(100):
-        sleep(1)
-        stlFile.status_msg = "Slicing: %d%%" % r
-        stlFile.save()
-
+    
     stlFile.status_msg = "Done: ready for printing"
     stlFile.save()
