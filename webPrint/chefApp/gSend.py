b95b0c4fd68775fb436389826859a83db278e7f7
C-o-r-E <can.of.tuna@gmail.com>
2013-12-25 00:46:22 +0000

    fixed class related issues

--- webPrint/chefApp/gSend.py
+++ webPrint/chefApp/gSend.py
@@ -18,19 +18,19 @@ class gcSender:
         def replycb(self, data):
                 print "reply--> " + data
                 
-                if device == None:
+                if self.device == None:
                         print "device is not defined"
                         
-                if elec.queueindex > 0: 
-                        pcnt = (100 * float(elec.queueindex) / len(elec.mainqueue)) 
+                if self.device.queueindex > 0: 
+                        pcnt = (100 * float(self.device.queueindex) / len(self.device.mainqueue)) 
                         print "%f complete" % pcnt
                                 
-                        if current_printer != None:
-                                current_printer.state = "Printing"
-                                current_printer.percent_complete = pcnt
-                                current_printer.save()
+                        if self.current_printer != None:
+                                self.current_printer.state = "Printing"
+                                self.current_printer.percent_complete = pcnt
+                                self.current_printer.save()
                 else:
-                        print "q index = %d" % elec.queueindex
+                        print "q index = %d" % self.queueindex
 
         def printGcodeFile(self, path_to_file):
                 sys.path.insert(0, '/home/corey/git/Printrun')
@@ -44,21 +44,21 @@ class gcSender:
                 plist = Printer.objects.all()
                 
                 if(len(plist) > 0):
-                        current_printer = plist[0]
+                        self.current_printer = plist[0]
                         
                 try:
                         #print "printing gcode for " + path_to_file
                         gcFile = open(path_to_file, "rU")
                         
-                        device=printcore.printcore('/dev/ttyUSB0', 115200)
-                        device.recvcb = self.replycb
-                        device.errorcb = self.errorcb 
+                        self.device = printcore.printcore('/dev/ttyUSB0', 115200)
+                        self.device.recvcb = self.replycb
+                        self.device.errorcb = self.errorcb 
                         
                         time.sleep(1)
                         
                         gcode = gcoder.GCode(gcFile)
                         
-                        device.startprint(gcode)                
+                        self.device.startprint(gcode)                
                         print "done printing?"
                         
                 except IOError:
