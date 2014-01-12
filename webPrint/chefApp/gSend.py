3c51c857c98aaf4555f05b786160763727632f1d
C-o-r-E <can.of.tuna@gmail.com>
2014-01-12 18:15:21 -0500

    switch to postgres, handle all the printcore callbacks

--- webPrint/chefApp/gSend.py
+++ webPrint/chefApp/gSend.py
@@ -12,11 +12,44 @@ class gcSender:
         # Callbacks
         #############
 
+	def tempcb(self, data):
+		print "temp --> " + data
+
         def errorcb(self, data):
                 print "error--> " + data
-                
+
+	def sendcb(self, data):
+		print "send --> " + data
+
+	def preprintsendcb(self, gline, nextgline):
+		if (gline != None) and (nextgline != None):
+			#print "preprintsend --> "
+			#print "\tline = " + gline.raw
+			#print "\tnextline = " + nextgline.raw
+			return gline
+
+	def printsendcb(self, gline):
+		if (gline != None):
+			#print "printsend --> "
+			print "\tline = " + gline.raw
+		
+			#Note: it seems that the total num of layers is broken in printcore 
+	def layerchangecb(self, data):
+		#print "layerchange --> %d" % (data, ) 
+		print "layerchange --> %d/%d" % (data, self.device.analyzer.num_layers()) 
+
+	def startcb(self, data):
+		print "start --> Resuming=" + str(data)
+
+	def endcb(self):
+		print "end()"
+	        #self.device.disconnect()
+
+	def onlinecb(self, data):
+		print "online --> " + data
+
         def replycb(self, data):
-                print "reply--> " + data
+                #print "reply--> " + data
                 
                 if self.device == None:
                         print "device is not defined"
@@ -38,7 +71,8 @@ class gcSender:
                 from printrun import printcore
 
                 #for testing
-                path_to_file = "/home/corey/test1.gcode"
+                path_to_file = "/home/corey/100actions.gcode"
+                #path_to_file = "/home/corey/test1.gcode"
                 
                 #default printer
                 plist = Printer.objects.all()
@@ -53,6 +87,14 @@ class gcSender:
                         self.device = printcore.printcore('/dev/ttyUSB0', 115200)
                         self.device.recvcb = self.replycb
                         self.device.errorcb = self.errorcb 
+
+			self.device.startcb = self.startcb
+			self.device.endcb = self.endcb
+			self.device.sendcb = self.sendcb
+			self.device.onlinecb = self.onlinecb
+			self.device.preprintsendcb = self.preprintsendcb
+			self.device.printsendcb = self.printsendcb
+			self.device.layerchangecb = self.layerchangecb
                         
                         time.sleep(1)
                         
@@ -60,7 +102,10 @@ class gcSender:
                         
                         self.device.startprint(gcode)                
                         print "done printing?"
+			
                         
                 except IOError:
                         print "Failed to open gcode file: " + path_to_file
                                 
+		except Exception:
+			print "Some kind of exception happened"
--- webPrint/webPrint/settings.py
+++ webPrint/webPrint/settings.py
@@ -14,15 +14,15 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'webPrintdb',
-        'USER': 'django_user',
-        'PASSWORD': 'django123',
-        'HOST': 'localhost',
-        'PORT': '',
-     },
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'webPrintdb',
+    #    'USER': 'django_user',
+    #    'PASSWORD': 'django123',
+    #    'HOST': 'localhost',
+    #    'PORT': '',
+    # },
 
-    'SQLite': {
+    #'SQLite': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
